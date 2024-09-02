93871e3387e8da723869d49effa2c15966078486
Enzo Glauber <enzojob@gmail.com>
2024-09-02 11:07:23 -0300

    enable ivy true

--- projects/core-angular/src/lib/button/button.module.ts
+++ projects/core-angular/src/lib/button/button.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { SpinnerModule } from "../spinner/spinner.module";
 import { ButtonComponent } from './button.component';
-import {SpinnerModule} from "../spinner/spinner.module";
 
 @NgModule({
   declarations: [ButtonComponent],
-    imports: [
-        CommonModule,
-        SpinnerModule
-    ],
+  imports: [
+    CommonModule,
+    SpinnerModule
+  ],
   exports: [ButtonComponent]
 })
 export class ButtonModule { }
